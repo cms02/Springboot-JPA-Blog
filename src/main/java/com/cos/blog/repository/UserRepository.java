f40f8406044ea4a1cd098cc37f48779583028e4a
cms02 <minoui1234@gmail.com>
2021-01-18 16:09:13 +0900

    spring security login page customizing

--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -10,11 +10,18 @@ import com.cos.blog.model.User;
 // 자동으로 bean등록이 된다
 //@Repository //생략 가능하다.
 public interface UserRepository extends JpaRepository<User, Integer>{
-	// JPA Naming 전략
-	// SELECT * FROM user WHERE username = ? AND password =?;
-
-	User findByUsernameAndPassword(String username, String password);
 	
+}
+
+
+
+
+
+
+// JPA Naming 전략
+// SELECT * FROM user WHERE username = ? AND password =?;
+
+//User findByUsernameAndPassword(String username, String password);
+
 //	@Query(value="SELECT * FROM user WHERE username =? AND password = ?", nativeQuery = true)
 //	User login(String username, String password);
-}
