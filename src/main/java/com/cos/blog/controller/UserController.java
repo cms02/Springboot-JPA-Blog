3bcd99b2cb9c71a53f552622747b9340c382f2c7
cms02 <minoui1234@gmail.com>
2021-01-18 14:27:08 +0900

    Revert "Revert "Revert "yml파일의 context-path 수정"""
    This reverts commit cca1f8425a10bd488ca7edfdb38a2db58a838cdf.

--- src/main/java/com/cos/blog/controller/UserController.java
+++ src/main/java/com/cos/blog/controller/UserController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-	@GetMapping("/joinForm")
+	@GetMapping("user/joinForm")
 	public String joinForm() {
 		
 		return "user/joinForm";
 	}
 	
-	@GetMapping("/loginForm")
+	@GetMapping("user/loginForm")
 	public String loginForm() {
 		
 		return "user/loginForm";
