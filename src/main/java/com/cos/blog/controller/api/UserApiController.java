d6a3ee5f1a7ea7cb2293d57c174dc84c442827d6
cms02 <minoui1234@gmail.com>
2021-01-18 15:29:03 +0900

    spring security

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -19,10 +19,6 @@ public class UserApiController {
 	@Autowired
 	private UserService UserService;
 	
-	@Autowired
-	private HttpSession session;
-	
-	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) { //username, password, email
 		System.out.println("UserApiController : save 호출됨");
@@ -32,14 +28,14 @@ public class UserApiController {
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); //자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
 	}
 	
-	@PostMapping("api/user/login")
-	public ResponseDto<Integer> login(@RequestBody User user){
-		System.out.println("UserApiController : login 호출됨");
-		User principal = UserService.로그인(user); //principal (접근주체)
-		
-		if(principal != null) {
-			session.setAttribute("principal", principal);
-		}
-		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
-	}
+//	@PostMapping("api/user/login")
+//	public ResponseDto<Integer> login(@RequestBody User user, HttpSession session){
+//		System.out.println("UserApiController : login 호출됨");
+//		User principal = UserService.로그인(user); //principal (접근주체)
+//		
+//		if(principal != null) {
+//			session.setAttribute("principal", principal);
+//		}
+//		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+//	}
 }
