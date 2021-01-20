461fba066b33e9e8166ab0bcf9661d8ec8463944
cms02 <minoui1234@gmail.com>
2021-01-20 16:42:55 +0900

    글 수정하기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -6,11 +6,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import com.cos.blog.dto.ResponseDto;
 import com.cos.blog.service.BoardService;
 
 @Controller
@@ -32,14 +30,22 @@ public class BoardController {
 	public String findById(@PathVariable int id, Model model) {
 		model.addAttribute("board", boardService.글상세보기(id));
 		return "board/detail";
-
 	}
+	
+	@GetMapping("/board/{id}/updateForm")
+	public String updateForm(@PathVariable int id, Model model) {
+		model.addAttribute("board", boardService.글상세보기(id));
+		return "board/updateForm";
+	}
+	
 
 	// USER 권한이 필요
 	@GetMapping("/board/saveForm")
 	public String saveForm() {
 		return "board/saveForm";
 	}
+	
+	
 	
 
 }
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -6,6 +6,9 @@ let index = {
 		$("#btn-delete").on("click", () => { //function(){}, ()=>{} this를 바인딩하기 위해서 !
 			this.deleteById();
 		});
+		$("#btn-update").on("click", () => { //function(){}, ()=>{} this를 바인딩하기 위해서 !
+			this.update();
+		});
 
 	},
 
@@ -30,7 +33,7 @@ let index = {
 		});
 	},
 	deleteById: function() {
-		var id = $("#id").text();
+		let id = $("#id").text();
 
 		$.ajax({
 			type: "DELETE",
@@ -40,6 +43,28 @@ let index = {
 			dataType: "json"
 		}).done(function(resp) {
 			alert("삭제가 완료되었습니다.");
+			location.href = "/";
+		}).fail(function() {
+			alert(JSON.stringify(error));
+		});
+	},
+	
+	update: function() {
+		let id = $("#id").val();
+		
+		let data = {
+			title: $("#title").val(),
+			content: $("#content").val()
+		};
+
+		$.ajax({
+			type: "PUT",
+			url: "/api/board/" + id,
+			data: JSON.stringify(data),
+			contentType: "application/json; charset=utf-8",
+			dataType: "json"
+		}).done(function(resp) {
+			alert("글수정이 완료되었습니다.");
 			location.href = "/";
 		}).fail(function() {
 			alert(JSON.stringify(error));
