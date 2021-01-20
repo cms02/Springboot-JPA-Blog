316791a109f87b6f457ff3599162a7c6ce01ecf7
cms02 <minoui1234@gmail.com>
2021-01-20 14:19:09 +0900

    글 목록 보기

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -1,19 +1,24 @@
 package com.cos.blog.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.cos.blog.config.auth.PrincipalDetail;
+import com.cos.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 	
+	@Autowired
+	private BoardService boardService;
+	
+	//컨트롤러에서 세션을 어떻게 찾는지?
 	//@AuthenticationPrincipal PrincipalDetail principal
 	@GetMapping({"", "/"})
-	public String index( ) { //컨트롤러에서 세션을 어떻게 찾는지?
-		// /WEB-INF/views/index.jsp
-		return "index";
+	public String index(Model model ) { 
+		 model.addAttribute("boards",boardService.글목록());
+		return "index"; //viewResolver 작동!!
 	}
 	
 	// USER 	권한이 필요
