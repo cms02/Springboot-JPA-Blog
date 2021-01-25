f3582b5f4886312351da80ef8797c667e51a3659
cms02 <minoui1234@gmail.com>
2021-01-25 13:43:31 +0900

    회원수정

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -2,10 +2,12 @@ package com.cos.blog.controller.api;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,12 @@ public class UserApiController {
 		System.out.println("UserApiController : save 호출됨");
 		UserService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1); //자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
+	}
+	
+	@PutMapping("/user")
+	public ResponseDto<Integer> update(@RequestBody User user){ //key = value, x-www-form-urlencoded
+		UserService.회원수정(user);
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
 	}
 	
 
