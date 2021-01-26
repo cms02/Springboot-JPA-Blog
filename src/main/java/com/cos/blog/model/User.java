97b8bcaad564c5d1d3639b5088927b3fe9b94128
cms02 <minoui1234@gmail.com>
2021-01-26 18:27:03 +0900

    카카오 회원가입 로그인

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -1,6 +1,8 @@
+  
 package com.cos.blog.model;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,32 +20,37 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder  //ORM -> Java(다른언어 포함) Object -> 테이블로 매핑해주는 기술
-@Entity //User 클래스가 MySQL에 테이블이 생성이 된다.
+@Builder // 빌더 패턴!!
+//ORM -> Java(다른언어) Object -> 테이블로 매핑해주는 기술
+@Entity // User 클래스가 MySQL에 테이블이 생성이 된다.
+// @DynamicInsert // insert시에 null인 필드를 제외시켜준다.
 public class User {
-
+	
 	@Id //Primary key
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
-	private int id; //시퀀스, auto_increment
-	
-	@Column(unique = true,  nullable = false, length = 30)
+	private int id; // 시퀀스, auto_increment
+	 
+	@Column(nullable = false, length = 100, unique = true) 
 	private String username; // 아이디
 	
-	@Column(nullable = false, length = 100) //123456 => 해쉬(비밀번호 암호화)
+	@Column(nullable = false, length = 100) // 123456 => 해쉬 (비밀번호 암호화)
 	private String password;
 	
 	@Column(nullable = false, length = 50)
-	private String email; //myEmail, my_email
-	
-	//@ColumnDefault("'user'")
-	//DB는 RoleType이라는 게 없다.
+	private String email; // myEmail, my_email
+
+	// @ColumnDefault("user")
+	// DB는 RoleType이라는 게 없다.
 	@Enumerated(EnumType.STRING)
-	private RoleType role; // Enum을 쓰는게 좋다. // admin, user, manager
+	private RoleType role; // Enum을 쓰는게 좋다. // ADMIN, USER
 	
-	@CreationTimestamp //시간이 자동 입력
+	private String oauth; // kakao, google
+	
+	// 내가 직접 시간을 넣으려면 Timestamp.valueOf(LocalDateTime.now())
+	@CreationTimestamp
 	private Timestamp createDate;
+	
 }
